d6e69291823d8bf0e32a9147e75b6e202d796c53
holodome <ilal.vinogradov@gmail.com>
2021-10-05 00:17:17 +0300

    repo cleanuo

--- src/lib/my_assert.cc
+++
@@ -1,7 +0,0 @@
-#include "lib/my_assert.hh"
-#include "lib/strings.hh"
-
-void 
-assert_msg(const char *expr, const char *filename, int line, const char *function) {
-    erroutf("%s:%d: %s: Assertion '(%s)' failed.\n", filename, line, function, expr);    
-}
